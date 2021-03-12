ee431699df18e0354fabd05c55e6be53f90b7f69
k.riabchenko <k.riabchenko@gamingtec.com>
2021-03-12 12:39:32 +0200

    docker fixes for master

--- app/MysqlConnector.php
+++ app/MysqlConnector.php
@@ -14,18 +14,18 @@ class MysqlConnector
 
     public static function getMaster(): PDO
     {
-        return self::getConnection($_ENV['MASTER_HOST']);
+        return self::getConnection($_ENV['MASTER_HOST'], $_ENV['MASTER_ROOT_PASS']);
     }
 
     public static function getSlave(): PDO
     {
-        return self::getConnection($_ENV['SLAVE_HOST']);
+        return self::getConnection($_ENV['SLAVE_HOST'], $_ENV['MASTER_SLAVE_PASS']);
     }
 
-    private static function getConnection(string $host): PDO
+    private static function getConnection(string $host, string $password): PDO
     {
         try {
-            $pdo = new PDO("mysql:host=$host;dbname=db", 'user', 'password', [
+            $pdo = new PDO("mysql:host=$host;dbname=db", 'root', $password, [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             ]);
         } catch (PDOException $e) {
